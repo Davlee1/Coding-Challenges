2661974c647c2eec074dfde668f03393d48fcd10
Davlee1 <dwlee0831@hotmail.com>
2022-11-05 23:53:35 -0400

    Update Cash register.js

--- FCC chanlenges/Javascript/Cash register.js
+++ FCC chanlenges/Javascript/Cash register.js
@@ -42,7 +42,7 @@ See below for an example of a cash-in-drawer array:
 
 function checkCashRegister(price, cash, cid) {
   let tid;
-  let change = cash - price;
+  let theChange = cash - price;
   let changeGiven = [  ["PENNY", 0],
   ["NICKEL", 0],
   ["DIME", 0],
@@ -59,63 +59,63 @@ function checkCashRegister(price, cash, cid) {
     }
 
 //checks if there is enough in the drawer or if enough cash was given
-    if(change > tid | change < 0){
+    if(theChange > tid | theChange < 0){
       return {status: "INSUFFICIENT_FUNDS", change: []}
     }
 
 //adds change in coins and bills to changeGiven
-  while(change > 0){
-    if(cid[b][0] =="ONE HUNDRED" && change >= cid[b][1]){
+  while(theChange > 0){
+    if(cid[b][0] =="ONE HUNDRED" && theChange >= cid[b][1]){
       changeGiven[8][1] += 100;
-      change -= 100;
+      theChange -= 100;
       cid[b][1] -= 100;
       continue;
     }
-    if(cid[b][0] =="TWENTY" && change >= cid[b][1]){
+    if(cid[b][0] =="TWENTY" && theChange >= cid[b][1]){
       changeGiven[7][1] += 20;
-      change -= 20;
+      theChange -= 20;
       cid[b][1] -= 20;
       continue;
     }
-    if(cid[b][0] =="TEN" && change >= cid[b][1]){
+    if(cid[b][0] =="TEN" && theChange >= cid[b][1]){
       changeGiven[6][1] += 10;
-      change -= 10;
+      theChange -= 10;
       cid[b][1] -= 10;
       continue;
     }
-    if(cid[b][0] =="FIVE" && change >= cid[b][1]){
+    if(cid[b][0] =="FIVE" && theChange >= cid[b][1]){
       changeGiven[5][1] += 5;
-      change -= 5;
+      theChange -= 5;
       cid[b][1] -= 5;
       continue;
     }
-    if(cid[b][0] =="ONE" && change >= cid[b][1]){
+    if(cid[b][0] =="ONE" && theChange >= cid[b][1]){
       changeGiven[4][1] += 1;
-      change -= 1;
+      theChange -= 1;
       cid[b][1] -= 1;
       continue;
     }
-    if(cid[b][0] =="QUARTER" && change >= cid[b][1]){
+    if(cid[b][0] =="QUARTER" && theChange >= cid[b][1]){
       changeGiven[3][1] += .25;
-      change -= .25;
+      theChange -= .25;
       cid[b][1] -= .25  ;
       continue;
     }
-    if(cid[b][0] =="DIME" && change >= cid[b][1]){
+    if(cid[b][0] =="DIME" && theChange >= cid[b][1]){
       changeGiven[2][1] += .1;
-      change -= .1;
+      theChange -= .1;
       cid[b][1] -= .1;
       continue;
     }
-    if(cid[b][0] =="NICKEL" && change >= cid[b][1]){
+    if(cid[b][0] =="NICKEL" && theChange >= cid[b][1]){
       changeGiven[1][1] += .05;
-      change -= .05;
+      theChange -= .05;
       cid[b][1] -= .5;
       continue;
     }
-    if(cid[b][0] =="PENNY" && change >= cid[b][1]){
+    if(cid[b][0] =="PENNY" && theChange >= cid[b][1]){
       changeGiven[0][1] += .01;
-      change -= .01;
+      theChange -= .01;
       cid[b][1] -= .01;
       continue;
     }
@@ -129,11 +129,19 @@ function checkCashRegister(price, cash, cid) {
     }
 
   }
-if(chang)
+
+  //resets change to initial value
+  theChange = cash - price;
+
+if(theChange == tid)
+  return {status: "CLOSED", change: changeGiven};
+else{
+  return {status: "OPEN", change: changeGiven};
+}
 
 
 
-  return change;
+  return theChange;
 }
 
 checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]);
